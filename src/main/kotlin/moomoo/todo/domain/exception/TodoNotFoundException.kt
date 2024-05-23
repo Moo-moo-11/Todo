e8bf15e9fd9e83af113df7c0b78cbedb399554a9
moomoo <anwns1211@gmail.com>
2024-05-23 20:50:32 +0900

    🔨 Refactor: 문구 수정

--- src/main/kotlin/moomoo/todo/domain/exception/TodoNotFoundException.kt
+++ src/main/kotlin/moomoo/todo/domain/exception/TodoNotFoundException.kt
@@ -2,4 +2,4 @@ package moomoo.todo.domain.exception
 
 class TodoNotFoundException(
     private val writer: String
-): RuntimeException("Todo not found with given name: $writer")
+): RuntimeException("Todo not found with given writer: $writer")
